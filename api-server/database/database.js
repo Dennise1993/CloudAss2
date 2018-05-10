baafc7f1daa046f6a3c3553150faa3bc198ca796
nichwn <nicholasb1@student.unimelb.edu.au>
2018-05-10 13:59:04 +1000

    Update database.js
    Bug fix for API server.

--- api-server/database/database.js
+++ api-server/database/database.js
@@ -6,9 +6,6 @@ const db = new (cradle.Connection)('couchdb', 5984, {
     auth: {
         username: process.env.COUCHDB_USER,
         password: process.env.COUCHDB_PASSWORD
-    },
-    request: {
-        timeout: 120000
     }
 }).database('tweets');
 
